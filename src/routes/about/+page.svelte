42238ef3ca45c3cb0713e16a9685ce1108b3276d
Kwame Gyampoh <kwame.gyampoh91@gmail.com>
2025-09-16 11:46:55 +0200

    images

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -23,7 +23,7 @@
 	<section
 		in:fly={{ y: 150, duration: 1700 }}
 		class="phone:h-[50vh] tablet:h-[60vh] laptop:h-[70vh] desktop:h-[70vh] relative mb-12 h-[60vh] items-center justify-center space-y-12 bg-cover bg-center bg-no-repeat"
-		style="background-image: url('/images/header-hero-img-4.jpeg');"
+		style="background-image: url('/images/header-hero-img-4.jpg');"
 	>
 		<!-- <Header {tabs} /> -->
 		<div class="mx-auto max-w-[1120px] min-w-[400px] px-3">
