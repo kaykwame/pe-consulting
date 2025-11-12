715cb662d53329329af9d740da9919fc69e0463c
Kwame Gyampoh <kwame.gyampoh91@gmail.com>
2025-11-12 23:33:53 +0100

    changed fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -262,7 +262,7 @@
 							</div>
 						</div>
 						<div class="tablet:w-4/5 laptop:w-4/5 desktop:w-4/5 mx-auto mb-2 w-full">
-							<div class="mb-1 text-center text-lg font-bold text-gray-900">
+							<div class="mb-1 text-center text-2xl font-bold text-gray-900">
 								{service}
 							</div>
 							{#each value.description_text as text}
